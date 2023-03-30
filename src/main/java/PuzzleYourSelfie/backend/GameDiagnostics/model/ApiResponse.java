ea1bf0be482990f4406169d341013ec6be74b0d5
mattergijz <matthijs.smets@hotmail.com>
2023-03-30 16:08:55 +0200

    Feat: api response model

---
+++ src/main/java/PuzzleYourSelfie/backend/GameDiagnostics/model/ApiResponse.java
@@ -0,0 +1,30 @@
+package PuzzleYourSelfie.backend.GameDiagnostics.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@Setter
+public class ApiResponse<Type> {
+    private HttpStatus code;
+    private Type payload;
+    private String message;
+
+//    public ApiResponse(HttpStatus code, Type payload) {
+//        this.code = code;
+//        this.payload = payload;
+//    }
+
+    public ApiResponse(HttpStatus code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public ApiResponse(HttpStatus code, Type payload, String message) {
+        this.code = code;
+        this.payload = payload;
+        this.message = message;
+    }
+}
