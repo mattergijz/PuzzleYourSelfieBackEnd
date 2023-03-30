e8235b36c2368e349d4f3d16d2b3ffc29c751203
mattergijz <matthijs.smets@hotmail.com>
2023-03-30 16:11:02 +0200

    Feat: created LevelDiagnostics model

--- src/main/java/PuzzleYourSelfie/backend/GameDiagnostics/Diagnostics/LevelDiagnostics.java
+++ src/main/java/PuzzleYourSelfie/backend/GameDiagnostics/Diagnostics/LevelDiagnostics.java
@@ -2,6 +2,7 @@ package PuzzleYourSelfie.backend.GameDiagnostics.Diagnostics;
 
 
 import PuzzleYourSelfie.backend.GameDiagnostics.Level.Level;
+import PuzzleYourSelfie.backend.GameDiagnostics.User.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,5 +27,21 @@ public class LevelDiagnostics {
     @JoinColumn(name = "level_id", referencedColumnName = "id")
     private Level level;
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 
+    private int timeLeft;
+
+    private int piecesLeft;
+
+    private boolean finished;
+
+    public LevelDiagnostics(Level level, User user, int timeLeft, int piecesLeft, boolean finished) {
+        this.level = level;
+        this.user = user;
+        this.timeLeft = timeLeft;
+        this.piecesLeft = piecesLeft;
+        this.finished = finished;
+    }
 }
