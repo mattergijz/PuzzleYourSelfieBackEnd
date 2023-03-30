dc701fbdeb90478c2b7baa7efed366d362a608bc
mattergijz <matthijs.smets@hotmail.com>
2023-03-30 03:20:16 +0200

    Fix: files cant be read from .jar file

--- src/main/java/PuzzleYourSelfie/backend/GameDiagnostics/DataLoader.java
+++ src/main/java/PuzzleYourSelfie/backend/GameDiagnostics/DataLoader.java
@@ -4,6 +4,7 @@ import PuzzleYourSelfie.backend.GameDiagnostics.Level.Level;
 import PuzzleYourSelfie.backend.GameDiagnostics.Level.LevelDAO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.Resource;
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.Scanner;
 
 
 @Component
@@ -25,12 +28,16 @@ public class DataLoader implements ApplicationRunner {
 
     private final ResourceLoader resourceLoader;
 
+    ApplicationContext context;
+
     //TODO: make sure file gets read in the dev environment as well als the production environment
-
-
-    public String loadFileFromJar() throws IOException {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("levels.json");
-        return String.valueOf(inputStream);
+    public String loadFileFromJar(ApplicationContext context) throws IOException {
+        URL resource = context.getResource("classpath:levels.json");
+        InputStream inputStream = resource.openStream();
+        Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8);
+        String fileContent = scanner.useDelimiter("\\A").next();
+        scanner.close();
+        return fileContent;
     }
 
     public String loadFileNormally() throws IOException {
@@ -47,7 +54,7 @@ public class DataLoader implements ApplicationRunner {
                 String currentFile = String.valueOf(DataLoader.class.getResource("DataLoader.class"));
                 Level[] levels1;
                 if (currentFile.startsWith("jar:")) {
-                    levels1 = mapper.readValue(loadFileFromJar(), Level[].class);
+                    levels1 = mapper.readValue(loadFileFromJar(context), Level[].class);
                 } else {
                     levels1 = mapper.readValue(loadFileNormally(), Level[].class);
                 }
