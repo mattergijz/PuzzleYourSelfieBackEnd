97a4492d1f8f4ed371baa0679100e4fd0b26eb75
mattergijz <matthijs.smets@hotmail.com>
2023-03-30 16:11:17 +0200

    Feat: get user by id

--- src/main/java/PuzzleYourSelfie/backend/GameDiagnostics/User/UserDAO.java
+++ src/main/java/PuzzleYourSelfie/backend/GameDiagnostics/User/UserDAO.java
@@ -1,4 +1,24 @@
 package PuzzleYourSelfie.backend.GameDiagnostics.User;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@AllArgsConstructor
 public class UserDAO {
+
+    private UserRepository userRepository;
+
+    public Optional<User> getById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    public void saveToDatabase(User user) {
+        userRepository.save(user);
+    }
+
 }
---
+++ src/main/java/PuzzleYourSelfie/backend/GameDiagnostics/UserNotFoundException.java
@@ -0,0 +1,5 @@
+package PuzzleYourSelfie.backend.GameDiagnostics;
+
+public class UserNotFoundException extends Exception{
+    private final String message = "User not found";
+}
