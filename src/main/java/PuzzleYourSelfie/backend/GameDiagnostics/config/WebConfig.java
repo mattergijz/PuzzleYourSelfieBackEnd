887d4cfc22837312eac12238c68b2396834b26f0
mattergijz <matthijs.smets@hotmail.com>
2023-04-12 09:04:43 +0200

    Feat: webconfig

---
+++ src/main/java/PuzzleYourSelfie/backend/GameDiagnostics/config/WebConfig.java
@@ -0,0 +1,7 @@
+package PuzzleYourSelfie.backend.GameDiagnostics.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+}
